a8d3a3acb7db192175867a8b26a8c507e94f7bc2
Mohammad Iqbal <ki925053@gmail.com>
2024-03-26 00:18:39 +0530

    Update service.js

--- src/appwrite/service.js
+++ src/appwrite/service.js
@@ -22,11 +22,106 @@ export class Service{
                 featuredImage,
                 status,
                 userid,
-
             })
         } catch (error) {
             throw error
         }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.database.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.database.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getonePost(slug){
+        try {
+            return await this.database.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try {
+            return await this.database.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    // file upload service and images upload
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            throw error
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
     }
 }
 const service = new Service();
